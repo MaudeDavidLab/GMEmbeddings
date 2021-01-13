e4cfc4a6dbf2f05ffa397963819479cb1b095e19
Austin Eaton <68047298+austineaton@users.noreply.github.com>
2021-01-13 11:15:05 -0800

    Add files via upload

--- GMEmbeddings/R/load_packages_from_lib.R
+++ GMEmbeddings/R/load_packages_from_lib.R
@@ -1,28 +1,10 @@
-# library(Biostrings)
-# library(rBLAST)
-# library(seqinr)
-# library(plyr)
-# library(data.table)
-#
-
-# install_or_load_pack <- function(pack){
-#   create.pkg <- pack[!(pack %in% installed.packages()[, "Package"])]
-#   if (length(create.pkg))
-#     install.packages(create.pkg, dependencies = TRUE)
-#   sapply(pack, require, character.only = TRUE)
-# }
-#
-#
-# packages <- c("Biostrings", "rBlast",  "seqinr", "plyr", "data.table")
-# install_or_load_pack(packages)
-
 load_packages_from_lib <- function(){
   library(Biostrings)
   library(rBLAST)
   library(seqinr)
   library(plyr)
   library(data.table)
-  library(pbapply)
+ # library(pbapply)
 }
 
 load_packages_from_lib()
