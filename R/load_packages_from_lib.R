f826977c548466b2cec21932dd7e6c19f26d7cd2
Austin Eaton <68047298+austineaton@users.noreply.github.com>
2021-01-20 11:35:20 -0800

    Add files via upload

---
+++ R/load_packages_from_lib.R
@@ -0,0 +1,15 @@
+load_packages_from_lib <- function(){
+  library(Biostrings)
+  library(rBLAST)
+  library(seqinr)
+  library(plyr)
+  #library(data.table)
+}
+
+load_packages_from_lib()
+
+# library(Biostrings)
+# library(rBLAST)
+# library(seqinr)
+# library(plyr)
+# library(data.table)
