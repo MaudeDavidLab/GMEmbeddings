e967abbe34a9348d94cf82c8f0159c042c67410d
Austin Eaton <68047298+austineaton@users.noreply.github.com>
2021-08-26 09:27:42 -0400

    Update embed_asv_table_new.R

--- R/embed_asv_table_new.R
+++ R/embed_asv_table_new.R
@@ -1,7 +1,6 @@
 
 #' @export
 getExampleSeqtab <- function(){
-  #seqtab <- utils::read.csv("/Library/Frameworks/R.framework/Versions/4.0/Resources/library/GMEmbeddings/extdata/seqtab_test.csv")#######################
   seqtab <- utils::read.csv(seqtab)
   rownames(seqtab) <- seqtab$X
   rownames(seqtab) <- gsub("X", "", rownames(seqtab))
@@ -32,12 +31,6 @@ getExampleSeqtab_Halfvarson <- function(){
 }
 
 
-#' #' @export
-# getExampleFasta <- function(){
-#   fasta_file <- fasta_file_name
-#   return(fasta_file)
-# }
-
 # This function takes in the best_hits datatable from the blast output, and creates an asv by embedding database asv
 # matrix where each element is 1 over the number of hits for that ASV in the entire database. Instead of breaking ties arbitrarily
 # we allow each ASV to hit multiple embedding ASV
@@ -59,8 +52,6 @@ getHitTransformation <- function(best_hits){
 #This function runs blast to find the closest hits to each query sequence in the embedding database
 #If blast has been run externally for increased speed, the function may take in the blast output table
 #If this function is taking more than a few minutes to run, consider running BLAST externally
-#"$blast_software_dir/blastn" -db "$blast_db/embedding_db_.07" -query "$data_dir/repseqs.fasta" -out "$out_dir/blast_hits.tsv"  -outfmt "6 qseqid sseqid qseq sseq evalue bitscore length pident"
-#cat "$out_dir/blast_hits.tsv" | sort -k1,1 -k5,5g -k6,6nr | sort -u -k1,1 --merge > "$out_dir/best_hits.tsv"
 getBestHits <- function(blast_hits, id_thresh = 99){
   #save best hits per query ASV
   print("Filtering best hits from blast output")
@@ -86,14 +77,6 @@ getEmbeddingHits <- function(fasta_file, blast_hits_file = NA, id_thresh = 99, o
   return(getBestHits(blast_hits))
 }
 
-
-# getFastaDF <- function(fasta_file){
-#   fasta <- read.fasta(fasta_file)
-#   asv_ids <- names(fasta)
-#   asv_seqs <- toupper(unlist(seqinr::getSequence(fasta, as.string = T)))
-#   fasta_df <- data.frame(asv_ids, row.names = asv_seqs)
-#   return(fasta_df)
-# }
 
 #For each query sequence, assigns the name of the closest embedding sequence. If there are multiple closest hits, splits the abundance
 #of the query sequence evenly among all closest hits
@@ -146,11 +129,8 @@ getFastaList <- function(fasta_file){
 
 
 EmbedAsvTable <- function(seqtab, fasta_file_name, blast_hits, embedding_file_name){
-
   best_hits = getBestHits(blast_hits = blast_hits, id_thresh = 99)
-
   seqtab <- embedSeqtab(seqtab, fasta_file = fasta_file_name, best_hits = best_hits, embedding_file_name)
-
   seqtab
 }
 
